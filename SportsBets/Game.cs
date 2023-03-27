e30169bbee4b203d8c4c905b8cd52c13bd2b24c2
Tonyo5 <antoniogjorgiev@yahoo.com>
2023-03-27 15:37:39 +0200

    Add project files.

---
+++ SportsBets/Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SportsBets
+{
+    public class Game
+    {
+        public Team Home { get; set; }
+        public Team Away { get; set; }
+        public string Code { get; set; }
+        public decimal[] Coeffietients { get; set; }
+
+        public Game()
+        {
+            Coeffietients = new decimal[3];
+        }
+        public override string ToString()
+        {
+            return string.Format("{0} - {1} vs. {2}",Code,Home,Away);
+        }
+    }
+}
